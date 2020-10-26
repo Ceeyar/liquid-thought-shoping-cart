a1cbfd5d59c372a079acaee6596626cb3d4c43ab
Siyabonga Ndovela <201490428@student.uj.ac.za>
2020-10-27 01:07:04 +0200

    created a datafile

---
+++ src/data/data.js
@@ -0,0 +1,136 @@
+
+export const products = [
+    {
+        "id": "5f95dc4b98e4ac59bf2b2fa4",
+        "index": 0,
+        "isSale": false,
+        "isAvailable": true,
+        "sku": "SKU001",
+        "image": "https://via.placeholder.com/600/56a8c2",
+        "description": "something here",
+        "unitPrice": 210,
+        "currency": "ZAR",
+        "name": "product 1"
+    },
+    {
+        "id": "5f95dc4bd15a338e4b8053ac",
+        "index": 1,
+        "isSale": false,
+        "isAvailable": true,
+        "sku": "SKU002",
+        "image": "https://via.placeholder.com/600/92c952",
+        "description": "something here",
+        "unitPrice": 221,
+        "currency": "ZAR",
+        "name": "product 2"
+    },
+    {
+        "id": "5f95dc4b5ef1bc55c0393a0b",
+        "index": 2,
+        "isSale": false,
+        "isAvailable": true,
+        "sku": "SKU003",
+        "image": "https://via.placeholder.com/600/8985dc",
+        "description": "something here",
+        "unitPrice": 232,
+        "currency": "ZAR",
+        "name": "product 3"
+    },
+    {
+        "id": "5f95dc4b3a2162fb46184120",
+        "index": 3,
+        "isSale": true,
+        "isAvailable": false,
+        "sku": "SKU004",
+        "image": "https://via.placeholder.com/600/8f209a",
+        "description": "something here",
+        "unitPrice": 243,
+        "currency": "ZAR",
+        "name": "product 4"
+    },
+    {
+        "id": "5f95dc4b654866c51ca71e1c",
+        "index": 4,
+        "isSale": false,
+        "isAvailable": true,
+        "sku": "SKU005",
+        "image": "https://via.placeholder.com/600/5e3a73",
+        "description": "something here",
+        "unitPrice": 254,
+        "currency": "ZAR",
+        "name": "product 5"
+    },
+    {
+        "id": "5f95dc4bd5991a9c0e2e59c8",
+        "index": 5,
+        "isSale": false,
+        "isAvailable": true,
+        "sku": "SKU006",
+        "image": "https://via.placeholder.com/600/474645",
+        "description": "something here",
+        "unitPrice": 265,
+        "currency": "ZAR",
+        "name": "product 6"
+    },
+    {
+        "id": "5f95dc4b8fddde120b803313",
+        "index": 6,
+        "isSale": false,
+        "isAvailable": false,
+        "sku": "SKU007",
+        "image": "https://via.placeholder.com/600/392537",
+        "description": "something here",
+        "unitPrice": 276,
+        "currency": "ZAR",
+        "name": "product 7"
+    },
+    {
+        "id": "5f95dc4b366230f4a5be7dfe",
+        "index": 7,
+        "isSale": false,
+        "isAvailable": false,
+        "sku": "SKU008",
+        "image": "https://via.placeholder.com/600/372c93",
+        "description": "something here",
+        "unitPrice": 287,
+        "currency": "ZAR",
+        "name": "product 8"
+    },
+    {
+        "id": "5f95dc4b67f270aeabf41560",
+        "index": 8,
+        "isSale": true,
+        "isAvailable": false,
+        "sku": "SKU009",
+        "image": "https://via.placeholder.com/600/c70a4d",
+        "description": "something here",
+        "unitPrice": 298,
+        "currency": "ZAR",
+        "name": "product 9"
+    },
+    {
+        "id": "5f95dc4b4dded9e100a2fc37",
+        "index": 9,
+        "isSale": true,
+        "isAvailable": true,
+        "sku": "SKU0010",
+        "image": "https://via.placeholder.com/600/4d564d",
+        "description": "something here",
+        "unitPrice": 309,
+        "currency": "ZAR",
+        "name": "product 10"
+    },
+    {
+        "id": "5f95dc4b3747c5ac63444403",
+        "index": 10,
+        "isSale": true,
+        "isAvailable": false,
+        "sku": "SKU0011",
+        "image": "https://via.placeholder.com/600/1e71a2",
+        "description": "something here",
+        "unitPrice": 320,
+        "currency": "ZAR",
+        "name": "product 11"
+    },
+    
+]
